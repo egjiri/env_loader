00c704c4f097da32476c2468382df89f101fb08f
Endri Gjiri <egjiri@gmail.com>
2014-01-01 13:09:29 -0500

    Fixes issue when there are values in the yml file that are integers

--- lib/env_loader.rb
+++ lib/env_loader.rb
@@ -1,4 +1,4 @@
-require "env_loader/version"
+require 'env_loader/version'
 require 'yaml'
 
 module EnvLoader
@@ -7,7 +7,7 @@ module EnvLoader
     if File.exists? env_yml_file
       if hash = YAML.load(File.open env_yml_file)
         hash.each do |key, value|
-          ENV[key.upcase] = value
+          ENV[key.upcase] = value.to_s if value
         end
       end
     end
--- lib/env_loader/version.rb
+++ lib/env_loader/version.rb
@@ -1,3 +1,3 @@
 module EnvLoader
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
