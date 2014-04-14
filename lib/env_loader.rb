618896d4a2ed3f1cec3d0b2507484e0b664db90d
Endri Gjiri <egjiri@gmail.com>
2014-04-13 20:36:42 -0400

    Saves hash objects as json in the ENV variable and provides a getter method to parse the json data back into a hash

--- lib/env_loader.rb
+++ lib/env_loader.rb
@@ -5,11 +5,34 @@ module EnvLoader
 
   def self.read(env_yml_file = File.join('config', 'env_variables.yml'))
     if File.exists? env_yml_file
-      if hash = YAML.load(File.open env_yml_file)
+      if hash = YAML.load_file(env_yml_file)
         hash.each do |key, value|
-          ENV[key.upcase] = value.to_s if value
+          if value
+            ENV[key.upcase] = format_value(value)
+          end
         end
       end
     end
   end
+
+  def self.get(key)
+    if value = ENV[key.to_s.upcase]
+      if value.match(/^\{.*\}$/)
+        JSON.parse(value).with_indifferent_access
+      else
+        value
+      end
+    end
+  end
+
+  private
+
+  def self.format_value(value)
+    if value.is_a? Hash
+      value = value.to_json
+    else
+      value = value.to_s
+    end
+  end
 end
+
