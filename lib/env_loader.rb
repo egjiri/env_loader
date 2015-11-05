be821d3211c59ad99b5bc851129effd1bfb39aa7
Endri Gjiri <egjiri@gmail.com>
2015-11-05 16:52:56 -0500

    Provides conventions to quickly setup monolith or microservice applications

--- lib/env_loader.rb
+++ lib/env_loader.rb
@@ -3,4 +3,32 @@ require 'env_loader/configurator'
 require 'uiux'
 
 module EnvLoader
+  # A .monolith-config.yml file must be placed at the root of the monolith
+  def self.setup_monolith
+    config(config_file: File.join('.monolith-config.yml'), config_subtree_key: environment)
+  end
+
+  # A .microservices-config.yml file must be placed at the parent directory of the microservice
+  def self.setup_microservice
+    subtree_key = environment
+    subtree_key += ".#{Rails.application.class.parent_name.underscore}" if defined?(Rails)
+    config(config_file: File.join('..', '.microservices-config.yml'), config_subtree_key: subtree_key)
+  end
+
+  # DEPRECATED (<= 0.2.0):
+  # Legacy setup which will be replaced by the above
+  def self.read(config_file = File.join('config', 'env_variables.yml'))
+    config(config_file: config_file)
+  end
+
+  private
+
+  def self.environment
+    Rails.env if defined?(Rails)
+  end
+
+  def self.config(config_file: nil, config_content: nil, config_subtree_key: nil)
+    configurator = Configurator.new(config_file: config_file, config_content: config_content, config_subtree_key: config_subtree_key)
+    configurator.expose_values_as_environment_variables if configurator
+  end
 end
--- spec/env_loader_spec.rb
+++ spec/env_loader_spec.rb
@@ -5,4 +5,25 @@ describe EnvLoader do
   it 'has a version number' do
     expect(EnvLoader::VERSION).not_to be_nil
   end
+
+  context '#setup_monolith' do
+    it 'should initialize Configurator using a .monolith-config.yml file at the root of the app' do
+      expect(EnvLoader::Configurator).to receive(:new).with(config_file: '.monolith-config.yml', config_content: nil, config_subtree_key: nil)
+      EnvLoader.setup_monolith
+    end
+  end
+
+  context '#setup_microservice' do
+    it 'should initialize Configurator using a .microservices-config.yml file located at the parent directory' do
+      expect(EnvLoader::Configurator).to receive(:new).with(config_file: '../.microservices-config.yml', config_content: nil, config_subtree_key: nil)
+      EnvLoader.setup_microservice
+    end
+  end
+
+  context '#read (DEPRECATED)' do
+    it 'should initialize Configurator using an env_variables.yml file located in the config directory' do
+      expect(EnvLoader::Configurator).to receive(:new).with(config_file: 'config/env_variables.yml', config_content: nil, config_subtree_key: nil)
+      EnvLoader.read
+    end
+  end
 end
